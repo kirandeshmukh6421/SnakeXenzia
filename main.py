fce2e19f2660967ac007b35b00707d0619b6b4f6
Kiran Deshmukh <64966083+kirandeshmukh6421@users.noreply.github.com>
2021-07-13 18:47:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,61 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from score import Scoreboard
+import time
+
+
+screen = Screen()
+screen.setup(width=600, height=610)
+
+# Set Window Position on Screen - Start
+width, height = screen.window_width(), screen.window_height()
+canvas = screen.getcanvas()
+
+left, top = 300, 20
+geom = '{}x{}+{}+{}'.format(width, height, left, top)
+canvas.master.geometry(geom)
+# End
+
+screen.bgcolor("black")
+screen.title("Snake Xenzia")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.reset()
+        snake.reset()
+        scoreboard.update_scoreboard()
+
+    # Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+            scoreboard.update_scoreboard()
+
+screen.exitonclick()
